12e34e7124282eecaeb381a879166b412b003f2b
kyle.wilson <kyle.wilson@foxmail.com>
2022-08-11 13:55:11 +0800

    optimize

--- P3070/main.cpp
+++ P3070/main.cpp
@@ -13,37 +13,33 @@ struct Matrix {
         for (int i = 0; i < MAX_N; ++i)
             for (int j = 0; j < MAX_N; ++j) data[i][j] = a[i][j];
     }
+    Matrix operator*(const Matrix &other) {
+        int i, j;
+        Matrix tmp;
+        for (i = 0; i < MAX_N; i++)
+            for (j = 0; j < MAX_N; j++)
+                for (int k = 0; k < MAX_N; k++) tmp.data[i][j] += data[i][k] * other.data[k][j];
+        for (i = 0; i < MAX_N; i++)
+            for (j = 0; j < MAX_N; j++) tmp.data[i][j] = tmp.data[i][j] % 10000;
+        return tmp;
+    }
 };
 
-void multi_matrix(Matrix &a, Matrix &b) {
-    int i, j;
-    Matrix tmp;
-
-    for (i = 0; i < MAX_N; i++)
-        for (j = 0; j < MAX_N; j++)
-            for (int k = 0; k < MAX_N; k++) {
-                tmp.data[i][j] += a.data[i][k] * b.data[k][j];
-            }
-
-    for (i = 0; i < MAX_N; i++)
-        for (j = 0; j < MAX_N; j++) {
-            a.data[i][j] = tmp.data[i][j] % 10000;
-        }
-}
-
-void pow_matrix(Matrix &a, int b, Matrix &ret) {
-
+Matrix pow_matrix(Matrix &a, int b) {
+    Matrix ret;
     for (int i = 0; i < MAX_N; i++) {
         ret.data[i][i] = 1;
     }
 
     while (b) {
         if (b & 1) {
-            multi_matrix(ret, a);
+            ret = ret * a;
         }
-        multi_matrix(a, a);
+        a = a * a;
         b >>= 1;
     }
+
+    return ret;
 }
 
 void display(Matrix &x) {
@@ -63,8 +59,9 @@ int main() {
 
     while (cin >> n && n != -1) {
         Matrix start(initial), ret;
-        pow_matrix(start, n, ret);
+        ret = pow_matrix(start, n);
         cout << ret.data[1][0] << endl;
     }
+
     return 0;
 }
