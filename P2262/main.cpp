d72d1408ab61756c77f2e011e494bd3b128c4e3a
kyle.wilson <kyle.wilson@foxmail.com>
2022-09-10 19:31:19 +0800

    fix

--- P2262/main.cpp
+++ P2262/main.cpp
@@ -8,11 +8,11 @@ bool num[MAX_N] = {false};
 int prime[MAX_M], cnt = 0;
 
 void get_prime() {
-    for (int i = 2; i <= MAX_N; i++) {
+    for (int i = 2; i < MAX_N; i++) {
         if (!num[i]) {
             prime[cnt++] = i;
         }
-        for (int j = 0; j < cnt && i * prime[j] <= MAX_N; ++j) {
+        for (int j = 0; j < cnt && i * prime[j] < MAX_N; ++j) {
             num[i * prime[j]] = true;
             if (i % prime[j] == 0) break;
         }
