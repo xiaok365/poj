74782b10b0c631939179ecc4d6d849884ffbcd25
kyle.wilson <kyle.wilson@foxmail.com>
2022-10-08 16:18:48 +0800

    optimize

--- P1182/main.cpp
+++ P1182/main.cpp
@@ -1,91 +1,61 @@
 #include <iostream>
-#include <fstream>
-#include <cstdio>
 
 using namespace std;
 
-const int MAXN = 50001;
+#define MAX_N 50001
 
-int parent[MAXN], parentRelation[MAXN], n;
+int father[MAX_N], relation[MAX_N], n;
 
-int findRoot(int a) {
+int find(int a) {
+    if (a == father[a]) return a;
 
-    if (a == parent[a]) return a;
-
-    int tempParent = parent[a];
-    parent[a] = findRoot(parent[a]);
+    int t = father[a];
+    father[a] = find(father[a]);
 
     // 路径压缩，a与root的关系=a与父亲的关系+父亲与root的关系
-    parentRelation[a] = (parentRelation[tempParent] + parentRelation[a]) % 3;
-    return parent[a];
+    relation[a] = (relation[t] + relation[a]) % 3;
+    return father[a];
 }
 
-void unionSet(int a, int b, int relation) {
-
-    int fa = findRoot(a);
-    int fb = findRoot(b);
-
-    if (fa == fb) {
-        return;
-    }
+void unite(int a, int b, int r) {
+    int fa = find(a), fb = find(b);
+    if (fa == fb) return;
     // fa的父亲指向fa
-    parent[fa] = fb;
+    father[fa] = fb;
     //a与fb的关系，a与b的关系+b与fb的关系
-    int a2fb = relation + parentRelation[b];
+    int a2fb = r + relation[b];
     // fa与fb的关系，a与fb的关系-a与fa的关系
-    parentRelation[fa] = (a2fb - parentRelation[a] + 3) % 3;
+    relation[fa] = (a2fb - relation[a] + 3) % 3;
 }
 
-
-bool check(int a, int b, int relation) {
-    if (a > n || b > n || a < 1 || b < 1) {
-        return false;
-    }
-    if (relation == 1 && a == b) {
-        return false;
-    }
-    int fa = findRoot(a);
-    int fb = findRoot(b);
-    if (fa == fb) {
-        return (parentRelation[a] - parentRelation[b] + 3) % 3 == relation;
+bool check(int a, int b, int r) {
+    if (a > n || b > n || a < 1 || b < 1 || r == 1 && a == b) return false;
+    if (find(a) == find(b)) {
+        return (relation[a] - relation[b] + 3) % 3 == r;
     }
     return true;
 }
 
 void init() {
-    for (int i = 0; i < MAXN; ++i) {
-        parent[i] = i;
-        parentRelation[i] = 0;
+    for (int i = 0; i < MAX_N; ++i) {
+        father[i] = i, relation[i] = 0;
     }
 }
 
 int main() {
-
-    ifstream cin("../a.in");
-    ofstream cout("../a.out");
-
-    int k, relation, a, b, lies = 0;
-
+    freopen("../a.in", "r", stdin);
+    int k, r, a, b, lies = 0;
     init();
-//    cin >> n >> k;
-    scanf("%d%d", &n, &k);
-
+    scanf("%d %d", &n, &k);
     for (int i = 0; i < k; ++i) {
-//        cin >> relation >> a >> b;
-        scanf("%d%d%d", &relation, &a, &b);
-        relation--;
-        if (check(a, b, relation)) {
-            unionSet(a, b, relation);
+        scanf("%d %d %d", &r, &a, &b);
+        r--;
+        if (check(a, b, r)) {
+            unite(a, b, r);
         } else {
             lies++;
-//            cout << "i=" << i << ", d=" << relation << ", a=" << a << ", b=" << b << endl;
         }
     }
-
-
-//    cout << lies << endl;
     printf("%d", lies);
-    cin.close();
-    cout.close();
     return 0;
 }
